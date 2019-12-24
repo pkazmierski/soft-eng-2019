bde4328b90bc73db8119121a9ee3248a10c8afab
Piotr Kaźmierski <217847@edu.p.lodz.pl>
2019-12-24 20:53:07 +0100

    Added drop-in authentication. Changed main activity message.

--- app/src/main/java/pl/se/fitnessapp/MainActivity.java
+++ app/src/main/java/pl/se/fitnessapp/MainActivity.java
@@ -2,7 +2,11 @@ package pl.se.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.amazonaws.mobile.client.AWSMobileClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtMainUserWelcomeMsg = findViewById(R.id.txtMainUserWelcomeMsg);
+        txtMainUserWelcomeMsg.setText("Welcome, " + AWSMobileClient.getInstance().getUsername() + ".");
     }
 }
