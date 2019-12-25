ecdd6e023bf57aa57394f307255f4cb55bfe64a4
Piotr Kaźmierski <217847@edu.p.lodz.pl>
2019-12-25 16:03:43 +0100

    Added navigation drawer support.

--- app/src/main/java/pl/se/fitnessapp/activities/MainActivity.java
+++ app/src/main/java/pl/se/fitnessapp/activities/MainActivity.java
@@ -1,24 +1,30 @@
 package pl.se.fitnessapp.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.TextView;
 
 import com.amazonaws.mobile.client.AWSMobileClient;
 
 import pl.se.fitnessapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends NavigationDrawerActivity {
 
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        @SuppressWarnings("ConstantConditions") @SuppressLint("InflateParams")
+        View contentView = inflater.inflate(R.layout.activity_main, null, false);
+        drawer.addView(contentView, 0);
 
         TextView txtMainUserWelcomeMsg = findViewById(R.id.txtMainUserWelcomeMsg);
+
         txtMainUserWelcomeMsg.setText("Welcome, " + AWSMobileClient.getInstance().getUsername() + ".");
     }
 }
---
+++ app/src/main/java/pl/se/fitnessapp/activities/NavigationDrawerActivity.java
@@ -0,0 +1,88 @@
+package pl.se.fitnessapp.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+
+import pl.se.fitnessapp.R;
+
+public class NavigationDrawerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawer;
+    NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.nav_dishes:
+                //startAnimatedActivity(new Intent(getApplicationContext(), ReplaceMe.class));
+                break;
+            case R.id.nav_exercises:
+                //startAnimatedActivity(new Intent(getApplicationContext(), ReplaceMe.class));
+                break;
+            case R.id.nav_gcalendar_export:
+                //startAnimatedActivity(new Intent(getApplicationContext(), ReplaceMe.class));
+                break;
+            case R.id.nav_gyms:
+                //startAnimatedActivity(new Intent(getApplicationContext(), ReplaceMe.class));
+                break;
+            case R.id.nav_personal_data:
+                //startAnimatedActivity(new Intent(getApplicationContext(), ReplaceMe.class));
+                break;
+            case R.id.nav_preferences:
+                //startAnimatedActivity(new Intent(getApplicationContext(), ReplaceMe.class));
+                break;
+            case R.id.nav_logoff:
+                //logoff rotine
+                break;
+            default:
+                throw new Error("No such navigation ID exists.");
+        }
+
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
