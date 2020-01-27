4909ba9351fd91069421fc142035288b311d3a79
preczu <ania.preczynska@gmail.com>
2020-01-27 20:43:47 +0100

    preferences init

---
+++ app/src/main/java/pl/se/fitnessapp/activities/PreferencesActivity.java
@@ -0,0 +1,23 @@
+package pl.se.fitnessapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+
+import pl.se.fitnessapp.R;
+
+public class PreferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+    }
+
+
+
+}
+
